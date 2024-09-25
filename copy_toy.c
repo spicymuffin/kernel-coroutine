0bc2ae2f05c52163c88f61d9ecf135f38c7a5935
spicymuffin <54279408+spicymuffin@users.noreply.github.com>
2024-09-25 21:36:49 +0900

    cache aligned

--- copy_toy.c
+++ copy_toy.c
@@ -2,61 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define OPCNT 1024 * 1024 * 10
-#define CPYBATCHSZ 32
-
 // util
 int random_in_range(int min, int max)
 {
     return rand() % (max - min + 1) + min;
-}
-
-typedef enum { INITIALIZING, EXECUTING, SWITCHING, UNUSED } coroutine_exec_state;
-
-typedef struct
-{
-    coroutine_exec_state exec_state;
-    int copy_indx;
-} coroutine_context;
-
-int access_pattern[OPCNT];
-
-char data_from[OPCNT];
-char data_to[OPCNT];
-
-void coroutine(coroutine_context* ctx, int* total_ops)
-{
-    switch (ctx->exec_state)
-    {
-    case INITIALIZING:
-        ctx->exec_state = EXECUTING;
-        // prefetch address, 0-read/1-write, locality (im not sure what this does)
-        __builtin_prefetch(data_from + ctx->copy_indx, 0, 1);
-        __builtin_prefetch(data_to + ctx->copy_indx, 1, 1);
-        return;
-
-    case EXECUTING:
-        (*total_ops)++;
-        // for (int i = 0; i < CPYBATCHSZ; i++)
-        // {
-        data_to[ctx->copy_indx] = data_from[ctx->copy_indx];
-        // }
-
-        ctx->exec_state = SWITCHING;
-        return;
-
-    case SWITCHING:
-    case UNUSED:
-        return;
-    }
-}
-
-void init_data()
-{
-    for (int i = 0; i < OPCNT; i++)
-    {
-        data_from[i] = random_in_range(0, 255);
-    }
 }
 
 // generates an array of access indices:
@@ -78,10 +27,102 @@ void generate_access_pattern(int* arr, int n)
     }
 }
 
+typedef enum { INITIALIZING, EXECUTING, SWITCHING, UNUSED } coroutine_exec_state;
+
+typedef struct
+{
+    coroutine_exec_state exec_state;
+    int copy_indx;
+} coroutine_context;
+
+// cache line size is 64 bytes
+
+int* array_from;
+int* array_to;
+
+size_t copy_batch = 16;
+
+size_t alignment = 64; // = cache line size
+size_t num_elements = 64 * 64 * 1024; // 16 MB
+
+void coroutine(coroutine_context* ctx, int* total_ops)
+{
+    switch (ctx->exec_state)
+    {
+    case INITIALIZING:
+        ctx->exec_state = EXECUTING;
+        // The GCC doc here specifies the usage of _buitin_prefetch.
+        // Third argument is perfect. If it is 0, compiler generates prefetchtnta (%rax) instruction
+        // If it is 1, compiler generates prefetcht2 (%rax) instruction
+        // If it is 2, compiler generates prefetcht1 (%rax) instruction
+        // If it is 3 (default), compiler generates prefetcht0 (%rax) instruction.
+        // If we vary third argument the opcode already changed accordingly.
+
+        // prefetch address, 0-read/1-write, locality
+        // locality:
+        // A value of zero means that the data has no temporal locality, so it need not be left in the cache after the access
+        // A value of three means that the data has a high degree of temporal locality and should be left in all levels of cache possible.
+        // (3 will put the data in L1 or L2 likely)
+        __builtin_prefetch(array_from + ctx->copy_indx, 0, 0);
+        return;
+
+    case EXECUTING:
+        (*total_ops)++;
+        for (int i = 0; i < copy_batch; i++)
+        {
+            array_to[ctx->copy_indx] = array_from[ctx->copy_indx];
+        }
+
+        ctx->exec_state = SWITCHING;
+        return;
+
+    case SWITCHING:
+        return;
+
+    case UNUSED:
+        return;
+    }
+}
+
+int init()
+{
+    // initialize the arrays (allocate alligned arrays)
+    if (posix_memalign((void**)&array_from, alignment, num_elements * sizeof(int)) != 0)
+    {
+        return 1; // allocation failed
+    }
+    // initialize the arrays (allocate alligned arrays)
+    if (posix_memalign((void**)&array_to, alignment, num_elements * sizeof(int)) != 0)
+    {
+        return 1; // allocation failed
+    }
+
+    for (int i = 0; i < num_elements; i++)
+    {
+        array_from[i] = random_in_range(0, 255);
+    }
+
+    // initialize or use the array
+    memset(array_to, 0, num_elements * sizeof(int));
+
+    return 0;
+}
+
+void cleanup()
+{
+    free(array_from);
+    free(array_to);
+}
+
 int main()
 {
-    init_data();
-    generate_access_pattern(access_pattern, OPCNT);
+    if (init() != 0)
+    {
+        printf("init failed\n");
+        cleanup();
+        return 1;
+    }
+
     printf("init seq completed\n");
 
     float sum = 0;
@@ -98,7 +139,7 @@ int main()
             for (int i = 0; i < num_coroutines; i++)
             {
                 ctxs[i].exec_state = INITIALIZING;
-                ctxs[i].copy_indx = access_pattern[copy_indx];
+                ctxs[i].copy_indx = copy_indx;
                 copy_indx++;
             }
 
@@ -117,10 +158,10 @@ int main()
                         }
                         else
                         {
-                            if (copy_indx < OPCNT)
+                            if (copy_indx < num_elements)
                             {
                                 ctxs[i].exec_state = INITIALIZING;
-                                ctxs[i].copy_indx = access_pattern[copy_indx];
+                                ctxs[i].copy_indx = copy_indx;
                                 copy_indx++;
                             }
                             else
@@ -137,12 +178,12 @@ int main()
             double elapsed_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
             double mops = total_ops / (elapsed_time * 1e6);
             // printf("Total ops with %d coroutines: %d, MOPS: %f\n", num_coroutines, total_ops, mops);
-            int indx = random_in_range(0, OPCNT);
+            int indx = random_in_range(0, num_elements);
             // printf("data @ %d: %c - %c\n", indx, data_from[indx], data_to[indx]);
 
-            for (int i = 0; i < OPCNT; i++)
+            for (int i = 0; i < num_elements; i++)
             {
-                data_to[indx] = '0';
+                array_to = 0;
             }
 
             sum += mops;
@@ -153,6 +194,6 @@ int main()
     }
 
     // printf("avg: %d\n", sum / cnt);
-
+    cleanup();
     return 0;
 }
